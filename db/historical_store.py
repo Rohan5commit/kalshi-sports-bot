63a42128ff2fe4c34fd856ed25bb87e1038e4fe0
Rohan Santhosh Kumar <181558744+Rohan5commit@users.noreply.github.com>
2026-06-17 21:37:40 +0530

    feat: add NBA team name→abbrev mapping for Kalshi enrichment join

--- db/historical_store.py
+++ db/historical_store.py
@@ -47,7 +47,20 @@ _MLB_NAME_TO_ABBREV = {
     "Toronto Blue Jays": "TOR", "Washington Nationals": "WAS",
 }
 
-_SPORT_ABBREV_MAP = {"NFL": _NFL_NAME_TO_ABBREV, "MLB": _MLB_NAME_TO_ABBREV}
+_NBA_NAME_TO_ABBREV = {
+    "Atlanta Hawks": "ATL", "Boston Celtics": "BOS", "Brooklyn Nets": "BKN",
+    "Charlotte Hornets": "CHA", "Chicago Bulls": "CHI", "Cleveland Cavaliers": "CLE",
+    "Dallas Mavericks": "DAL", "Denver Nuggets": "DEN", "Detroit Pistons": "DET",
+    "Golden State Warriors": "GSW", "Houston Rockets": "HOU", "Indiana Pacers": "IND",
+    "Los Angeles Clippers": "LAC", "Los Angeles Lakers": "LAL", "Memphis Grizzlies": "MEM",
+    "Miami Heat": "MIA", "Milwaukee Bucks": "MIL", "Minnesota Timberwolves": "MIN",
+    "New Orleans Pelicans": "NOP", "New York Knicks": "NYK", "Oklahoma City Thunder": "OKC",
+    "Orlando Magic": "ORL", "Philadelphia 76ers": "PHI", "Phoenix Suns": "PHX",
+    "Portland Trail Blazers": "POR", "Sacramento Kings": "SAC", "San Antonio Spurs": "SAS",
+    "Toronto Raptors": "TOR", "Utah Jazz": "UTA", "Washington Wizards": "WAS",
+}
+
+_SPORT_ABBREV_MAP = {"NFL": _NFL_NAME_TO_ABBREV, "MLB": _MLB_NAME_TO_ABBREV, "NBA": _NBA_NAME_TO_ABBREV}
 
 _SB_CLIENT = None
 _SB_LOCK = threading.Lock()
